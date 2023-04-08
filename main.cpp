2dfe3ace8ebb700fd7c7d8e6a6a0009b0d24e00b
BLooDBRothER <computertechnology446@gmail.com>
2023-04-08 17:35:10 +0530

    Add command line args for input

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <string.h>
 #include "resolver.h"
+
+#define INFO_MSG "Provide with any one of the following option\n<name> - To convert domain name -> IP\n<ip> - To convert IP -> name"
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
     Resolver resolver;
-
+    if(argc < 2){
+        cout << INFO_MSG;
+        return 0;
+    }
     char input[100];
     char protocol[30];
 
-    cout << "Enter URL: ";
-    cin >> input;
-    cout << endl << "Enter protocol: ";
-    cin >> protocol;
-
-    resolver.resolveIp(input, protocol);
-    resolver.displayIp();
+    if(!strcmp(argv[1], "name")){
+        cout << "Enter URL: ";
+        cin >> input;
+        cout << endl << "Enter protocol: ";
+        cin >> protocol;
+        resolver.resolveIp(input, protocol);
+    }
+    else if(!strcmp(argv[1], "ip")){
+        cout << "Enter IP Address: ";
+        cin >> input;
+        resolver.resolveName(input);
+    }
+    else{
+        cout << INFO_MSG;
+    }
 }
