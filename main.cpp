9012019ac6a8623b894133384076005af67eb8f5
BLooDBRothER <computertechnology446@gmail.com>
2023-04-08 18:42:01 +0530

    Refactor code

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 #include <string.h>
 #include "resolver.h"
 
-#define INFO_MSG "Provide with any one of the following option\n<name> - To convert domain name -> IP\n<ip> - To convert IP -> name"
+#define HELP_MSG "<name> - To convert domain name -> IP\n<ip> - To convert IP -> name"
+#define INFO_MSG "Provide with any one of the following option\n"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
     Resolver resolver;
-    if(argc < 2){
-        cout << INFO_MSG;
-        return 0;
-    }
     char input[100];
     char protocol[30];
 
@@ -27,7 +24,10 @@ int main(int argc, char *argv[]){
         cin >> input;
         resolver.resolveName(input);
     }
+    else if(!strcmp(argv[1], "help")){
+        cout << HELP_MSG;
+    }
     else{
-        cout << INFO_MSG;
+        cout << INFO_MSG << HELP_MSG;
     }
 }
